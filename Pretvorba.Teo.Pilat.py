e1699c3d0a7e2134c227f9e3563fbee119b7bc10
TeoPilat <teo.pilat1@gmail.com>
2025-10-13 13:18:24 +0200

    Pretvorba.Teo.Pilat.py

--- Pretvorba.Teo.Pilat.py
+++ Pretvorba.Teo.Pilat.py
@@ -1,17 +1,15 @@
 #izbornik za kalkulator pretvorbe mjernih jedinica
 print("izbornik za kalkulator pretvorbe mjernih jedinica")
 print("----------------------")
+print("izaberite opciju:")
+print("0.izlaz")
+print("1.pretvaranje volti")
+print("2. pretvaranje ampera")
+print("3.pretvaranje ohma")
+print("----------------------")
 while True:
-    print("izaberite opciju:")
-    print("0.izlaz")
-    print("1.pretvaranje volti")
-    print("2. pretvaranje ampera")
-    print("3.pretvaranje ohma")
-   
-    print("----------------------")
-   
     opcija=float(input("izaberite opciju (0 / 1 / 2 / 3):"))
-#struktura grananja
+    #struktura grananja
     if opcija == 1:
        Napon=float(input("upiši napon u voltima: "))
 
@@ -25,7 +23,7 @@ while True:
     #pretvorba ohma
     elif opcija == 3:
         Otpor=float(input("upiši otpor u ohmima: "))
-        R=Otpor/1000
+        R=Otpor*1000
         print(f"otpor u kiloohmima je: {R} kOhm")
     elif opcija ==0:
         print("izlaz iz programa")
